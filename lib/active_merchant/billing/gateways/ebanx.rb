8c07eb5868f92b060a6531f1c918de54c179983b
Silvio Fernandes <silvio.fernandes@resultadosdigitais.com.br>
2017-09-05 22:00:14 -0300

    rollback payment name

--- lib/active_merchant/billing/gateways/ebanx.rb
+++ lib/active_merchant/billing/gateways/ebanx.rb
@@ -138,7 +138,7 @@ module ActiveMerchant #:nodoc:
       end
 
       def add_customer_data(post, payment, options)
-        post[:payment][:name] = payment.try(:name) || options[:name]
+        post[:payment][:name] = payment.name
         post[:payment][:email] = options[:email] || "unspecified@example.com"
         post[:payment][:document] = options[:document]
         post[:payment][:birth_date] = options[:birth_date] if options[:birth_date]
@@ -146,7 +146,7 @@ module ActiveMerchant #:nodoc:
 
       def add_customer_responsible_person(post, payment,  options)
         post[:payment][:responsible] = {}
-        post[:payment][:responsible][:name] = payment.try(:name) || options[:name]
+        post[:payment][:responsible][:name] = payment.name
         post[:payment][:responsible][:document] = options[:document]
         post[:payment][:responsible][:birth_date] = options[:birth_date] if options[:birth_date]
       end
